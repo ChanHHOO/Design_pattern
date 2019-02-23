fcd07ae1c084edee03f55e28b348757739ec8cad
ChanHO <hpyho33@kookmin.ac.kr>
2019-02-23 19:03:40 +0900

    insert into tables

--- myweb/urls.py
+++ myweb/urls.py
@@ -12,11 +12,15 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+
+
+    r'^' < localhost:8000
 """
 from django.contrib import admin
 from django.conf.urls import url
 from home import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index)
+    url(r'^$', views.index),
+    url(r'^test', views.test),
     ]
